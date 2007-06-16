e3e5b36afabd1eb08e65dabb09922421f26a96f1
MATSUFUJI Hideharu <matsufuji@iteman.jp>
2007-06-16 02:33:32 +0000

    - エディットパートの取得方法を修正した。

--- com.piece_framework.piece_ide/src/com/piece_framework/piece_ide/flow_designer/ui/property/FlowElementLabelProvider.java
+++ com.piece_framework.piece_ide/src/com/piece_framework/piece_ide/flow_designer/ui/property/FlowElementLabelProvider.java
@@ -1,86 +1,89 @@
 package com.piece_framework.piece_ide.flow_designer.ui.property;
 
-import java.util.Iterator;
-
-import org.eclipse.jface.util.Assert;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.viewers.LabelProvider;
 import org.eclipse.jface.viewers.StructuredSelection;
-import org.eclipse.ui.views.properties.tabbed.ITypeMapper;
 
-import com.piece_framework.piece_ide.flow_designer.ui.editpart.ActionStateEditPart;
-import com.piece_framework.piece_ide.flow_designer.ui.editpart.FinalStateEditPart;
-import com.piece_framework.piece_ide.flow_designer.ui.editpart.FlowEditPart;
-import com.piece_framework.piece_ide.flow_designer.ui.editpart.InitialStateEditPart;
-import com.piece_framework.piece_ide.flow_designer.ui.editpart.ViewStateEditPart;
+import com.piece_framework.piece_ide.flow_designer
+            .ui.editpart.ActionStateEditPart;
+import com.piece_framework.piece_ide.flow_designer
+            .ui.editpart.FinalStateEditPart;
+import com.piece_framework.piece_ide.flow_designer
+            .ui.editpart.FlowEditPart;
+import com.piece_framework.piece_ide.flow_designer
+            .ui.editpart.InitialStateEditPart;
+import com.piece_framework.piece_ide.flow_designer
+            .ui.editpart.ViewStateEditPart;
 
+/**
+ * フローのラベルプロバイダー.
+ * 
+ * @author MATSUFUJI Hideharu
+ * @version 0.1.0
+ * @since 0.1.0
+ * 
+ */
 public class FlowElementLabelProvider extends LabelProvider {
 
-    private ITypeMapper fMapper;
-    
+    /**
+     * コンストラクタ.
+     * マッパーを生成する。
+     * 
+     */
     public FlowElementLabelProvider() {
         super();
-        fMapper = new FlowElementTypeMapper();
     }
 
+    /**
+     * セクションのタイトルとなる文字列を返す.
+     * 
+     * @param elements 選択されたオブジェクト
+     * @return セクションのタイトル文字列
+     * @see org.eclipse.jface.viewers.LabelProvider#getText(java.lang.Object)
+     */
     @Override
     public String getText(Object elements) {
+        String text = null;
+        
         if (elements == null || elements.equals(StructuredSelection.EMPTY)) {
             return "フロー";
         }
-        final boolean multiple[] = {false};
-        final Object object = getObject(elements, multiple);
-        if (object == null || ((IStructuredSelection) elements).size() > 1) {
-            return ((IStructuredSelection) elements).size() + " items selected";//$NON-NLS-1$
-        } else {
+        
+        Object object = getSelectionObject(elements);
+        
+        if (object != null) {
             if (object instanceof ActionStateEditPart) {
-                return "アクションステート";
+                text = "アクションステート";
             } else if (object instanceof ViewStateEditPart) {
-                return "ビューステート";
+                text = "ビューステート";
             } else if (object instanceof InitialStateEditPart) {
-                return "イニシャルステート";
+                text = "イニシャルステート";
             } else if (object instanceof FinalStateEditPart) {
-                return "ファイナルステート";
+                text = "ファイナルステート";
             } else if (object instanceof FlowEditPart) {
-                return "フロー";
+                text = "フロー";
+            } else {
+                text = "不明";
             }
-            String name = fMapper.mapType(object).getName();
-            return name.substring(name.lastIndexOf('.') + 1);
         }
+        
+        return text;
     }
     
-    private Object getObject(Object objects, boolean multiple[]) {
-        Assert.isNotNull(objects);
+    /**
+     * セレクションオブジェクトからエディットパートを取得する.
+     * 複数選択されている場合は、最初に選択したオブジェクトの
+     * エディットパートを返す。
+     * 
+     * @param objects セレクションオブジェクト
+     * @return エディットパート
+     */
+    private Object getSelectionObject(Object objects) {
         Object object = null;
         if (objects instanceof IStructuredSelection) {
             IStructuredSelection selection = (IStructuredSelection) objects;
             object = selection.getFirstElement();
-            if (selection.size() == 1) {
-                // one element selected
-                multiple[0] = false;
-                return object;
-            }
-            // multiple elements selected
-            multiple[0] = true;
-            Class firstClass = fMapper.mapType(object);
-            // determine if all the objects in the selection are the same type
-            if (selection.size() > 1) {
-                for (Iterator i = selection.iterator(); i.hasNext();) {
-                    Object next = i.next();
-                    Class nextClass = fMapper.mapType(next);
-                    if (!nextClass.equals(firstClass)) {
-                        // two elements not equal == multiple selected unequal
-                        multiple[0] = false;
-                        object = null;
-                        break;
-                    }
-                }
-            }
-        } else {
-            multiple[0] = false;
-            object = objects;
         }
         return object;
     }
-    
 }
