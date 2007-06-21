d8bc2a07f21efde2d3094d4ef97514ee89ac17d0
MATSUFUJI Hideharu <matsufuji@iteman.jp>
2007-06-21 02:05:15 +0000

    - ステートに「イベント」タブを追加した。

---
+++ com.piece_framework.piece_ide/src/com/piece_framework/piece_ide/flow_designer/ui/property/StateEventSection.java
@@ -0,0 +1,11 @@
+package com.piece_framework.piece_ide.flow_designer.ui.property;
+
+import org.eclipse.ui.views.properties.tabbed.AbstractPropertySection;
+
+public class StateEventSection extends AbstractPropertySection {
+
+    public StateEventSection() {
+        // TODO 自動生成されたコンストラクター・スタブ
+    }
+
+}
