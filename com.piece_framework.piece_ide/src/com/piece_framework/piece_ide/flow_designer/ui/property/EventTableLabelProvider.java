d6935d6d68687f8d491aee77c6f794f68b9bfaf9
MATSUFUJI Hideharu <matsufuji@iteman.jp>
2007-06-26 02:46:44 +0000

    - 新規追加。

---
+++ com.piece_framework.piece_ide/src/com/piece_framework/piece_ide/flow_designer/ui/property/EventTableLabelProvider.java
@@ -0,0 +1,68 @@
+package com.piece_framework.piece_ide.flow_designer.ui.property;
+
+import org.eclipse.jface.viewers.ITableLabelProvider;
+import org.eclipse.jface.viewers.LabelProvider;
+import org.eclipse.swt.graphics.Image;
+
+import com.piece_framework.piece_ide.flow_designer.model.Event;
+import com.piece_framework.piece_ide.flow_designer.model.EventHandler;
+import com.piece_framework.piece_ide.flow_designer.model.State;
+
+public class EventTableLabelProvider extends LabelProvider implements
+        ITableLabelProvider {
+
+    public static final int EVENT_COLUMN = 0;
+    public static final int NEXT_STATE_COLUMN = 1;
+    public static final int EVENT_HANDLER_COLUMN = 2;
+    public static final int GUARD_COLUMN = 3;
+    
+    public Image getColumnImage(Object element, int columnIndex) {
+        // TODO 自動生成されたメソッド・スタブ
+        return null;
+    }
+    
+    public String getColumnText(Object element, int columnIndex) {
+        Event event = (Event) element;
+        
+        switch (columnIndex) {
+        case EVENT_COLUMN:
+            if (event.getName() != null) {
+                return event.getName();
+            }
+            break;
+        case NEXT_STATE_COLUMN:
+            State nextState = event.getNextState();
+            if (nextState != null && nextState.getName() != null) {
+                return nextState.getName();
+            }
+            break;
+        case EVENT_HANDLER_COLUMN:
+            EventHandler eventHandler = event.getEventHandler();
+            if (eventHandler != null) {
+                String className = eventHandler.getClassName();
+                String methodName = eventHandler.getMethodName();
+                if (className != null && methodName != null) {
+                    return className + ":" + methodName;
+                } else if (methodName != null) {
+                    return ":" + methodName;
+                }
+            }
+            break;
+        case GUARD_COLUMN:
+            EventHandler guardEventHandler = event.getGuardEventHandler();
+            if (guardEventHandler != null) {
+                String className = guardEventHandler.getClassName();
+                String methodName = guardEventHandler.getMethodName();
+                if (className != null && methodName != null) {
+                    return className + ":" + methodName;
+                } else if (methodName != null) {
+                    return ":" + methodName;
+                }
+            }
+            break;
+        }
+        
+        return "";
+    }
+
+}
