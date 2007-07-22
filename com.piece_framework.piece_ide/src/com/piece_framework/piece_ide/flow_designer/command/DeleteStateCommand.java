64d0a573b7bbfa5306188b2c7762f0bf4fbac7c8
MATSUFUJI Hideharu <matsufuji@iteman.jp>
2007-07-22 11:31:43 +0000

    - ステート削除時、自身が遷移先となる遷移イベントも削除するようにした。   (Ticket #43)

--- com.piece_framework.piece_ide/src/com/piece_framework/piece_ide/flow_designer/command/DeleteStateCommand.java
+++ com.piece_framework.piece_ide/src/com/piece_framework/piece_ide/flow_designer/command/DeleteStateCommand.java
@@ -20,9 +20,59 @@ import com.piece_framework.piece_ide.flow_designer.model.State;
  */
 public class DeleteStateCommand extends Command {
 
+    /**
+     * ステート・イベント セットクラス.
+     * ステートとイベントを1対1の関係で保持するクラス。
+     * 
+     * @author MATSUFUJI Hideharu
+     * @version 0.1.0
+     * @since 0.1.0
+     *
+     */
+    private class StateEvent {
+        private State fState;
+        private Event fEvent;
+        
+        /**
+         * ステートを設定する.
+         * 
+         * @param state ステート
+         */
+        void setState(State state) {
+            fState = state;
+        }
+
+        /**
+         * イベントを設定する.
+         * 
+         * @param event イベント
+         */
+        void setEvent(Event event) {
+            fEvent = event;
+        }
+
+        /**
+         * イベントを追加する.
+         * 
+         */
+        void addEvent() {
+            fState.addEvent(fEvent);
+        }
+        
+        /**
+         * イベントを削除する.
+         * 
+         */
+        void removeEvent() {
+            fState.removeEvent(fEvent);
+        }
+    }
+    
     private Flow fFlow;
     private State fState;
     private List<Event> fEvents;
+    private List<StateEvent> fToOwnState;
+    
     /**
      * コンストラクタ.
      * 
@@ -32,12 +82,30 @@ public class DeleteStateCommand extends Command {
     public DeleteStateCommand(Flow flow, State state) {
         fFlow = flow;
         fState = state;
+        
         fEvents = new ArrayList<Event>();
-        fEvents.addAll(state.getEventList());
+        fEvents.addAll(fState.getEventList());
+        
+        fToOwnState = new ArrayList<StateEvent>();
+        for (State stateToOwnState : fFlow.getStateListToOwnState(fState)) {
+            for (Event event : stateToOwnState.getTransitionEventList()) {
+                if (event.getNextState() == fState) {
+                    StateEvent stateEvent = new StateEvent();
+                    stateEvent.setState(stateToOwnState);
+                    stateEvent.setEvent(event);
+                    fToOwnState.add(stateEvent);
+                }
+            }
+        }
     }
 
     /**
      * ステート削除コマンドを実行する.
+     * ステートの削除は以下の手順で行われる。<br>
+     * 1.削除対象ステートが保持するイベントをすべて削除する。<br>
+     * 　(遷移の線を削除するため)<br>
+     * 2.フローからステートを削除する。<br>
+     * 3.削除対象ステートが遷移先となっている遷移ステートを削除する。<br>
      * 
      * @see org.eclipse.gef.commands.Command#execute()
      */
@@ -47,10 +115,17 @@ public class DeleteStateCommand extends Command {
             fState.removeEvent(event);
         }
         fFlow.removeState(fState);
+        for (StateEvent stateEvent : fToOwnState) {
+            stateEvent.removeEvent();
+        }
     }
 
     /**
      * 実行したステート削除コマンドを元に戻す.
+     * 削除したステートの復元は以下の手順で行われる。<br>
+     * 1.削除対象ステートをフローに追加する。<br>
+     * 2.削除対象ステートのイベントを追加する。<br>
+     * 3.削除対象ステートが遷移先となっているイベントを追加する。<br>
      * 
      * @see org.eclipse.gef.commands.Command#undo()
      */
@@ -59,6 +134,9 @@ public class DeleteStateCommand extends Command {
         fFlow.addState(fState);
         for (Event event : fEvents) {
             fState.addEvent(event);
+        }
+        for (StateEvent stateEvent : fToOwnState) {
+            stateEvent.addEvent();
         }
     }
 }
