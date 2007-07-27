6697147d7f69da0bf74d8893e2b96c0d29b59907
MATSUFUJI Hideharu <matsufuji@iteman.jp>
2007-07-27 00:42:25 +0000

    - FlowMapper#getYAML メソッドの仮実装を行った。

--- com.piece_framework.piece_ide/src/com/piede_framework/piece_ide/flow_designer/mapper/FlowMapper.java
+++ com.piece_framework.piece_ide/src/com/piede_framework/piece_ide/flow_designer/mapper/FlowMapper.java
@@ -1,5 +1,20 @@
 package com.piede_framework.piece_ide.flow_designer.mapper;
 
+import com.piece_framework.piece_ide.flow_designer.model.Flow;
+
 public class FlowMapper {
 
+    public static String getYAML(Flow flow) {
+        return 
+            "firstState: DisplayForm1\n\n"
+          + "lastState:\n"
+          + "  name: DisplayForm1\n"
+          + "  view: Form1\n"
+          + "  activity:\n"
+          + "    method: ActionClass:doActivityOnDisplayForm1\n"
+          + "  entry:\n"
+          + "    method: ActionClass:doEntryOnDisplayForm1\n"
+          + "  exit:\n"
+          + "    method: ActionClass:doExitOnDisplayForm1\n";
+    }
 }
