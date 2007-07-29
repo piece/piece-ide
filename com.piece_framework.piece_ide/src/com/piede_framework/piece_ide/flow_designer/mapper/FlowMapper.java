861fc415d9da978e2dccbdd9327098c5deb9507e
MATSUFUJI Hideharu <matsufuji@iteman.jp>
2007-07-29 04:44:25 +0000

    - アクション・ビューステートの YAML 出力を実装した。

--- com.piece_framework.piece_ide/src/com/piede_framework/piece_ide/flow_designer/mapper/FlowMapper.java
+++ com.piece_framework.piece_ide/src/com/piede_framework/piece_ide/flow_designer/mapper/FlowMapper.java
@@ -1,7 +1,9 @@
 // $Id$
 package com.piede_framework.piece_ide.flow_designer.mapper;
 
+import java.util.ArrayList;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.ho.yaml.Yaml;
@@ -21,14 +23,9 @@ import com.piece_framework.piece_ide.flow_designer.model.State;
  * @since 0.1.0
  * 
  */
-public final class FlowMapper {
+public class FlowMapper {
 
-    /**
-     * コンストラクタ.
-     * 
-     */
-    private FlowMapper() {
-    }
+    private Flow fFlow;
     
     /**
      * 指定されたFlowをYAMLで出力する.
@@ -36,62 +33,147 @@ public final class FlowMapper {
      * @param flow フロー
      * @return YAML
      */
-    public static String getYAML(Flow flow) {
-        StringBuffer yamlBuffer = new StringBuffer();
+    public String getYAML(Flow flow) {
+        fFlow = flow;
         
-        Yaml.config.setMinimalOutput(true);
+        Map firstStateMap = new LinkedHashMap();
+        Map finalStateMap = new LinkedHashMap();
+        Map viewStateMap = new LinkedHashMap();
+        List viewStateList = new ArrayList();
+        Map actionStateMap = new LinkedHashMap();
+        List actionStateList = new ArrayList();
         
-        for (State state : flow.getStateList()) {
-            Map stateMap = new LinkedHashMap();
+        for (State state : fFlow.getStateList()) {
             if (state.getType() == State.INITIAL_STATE) {
                 for (Event event : state.getEventList()) {
                     if (event.getType() == Event.TRANSITION_EVENT) {
-                        stateMap.put("firstState", event.getNextState().getName());
+                        firstStateMap.put("firstState", event.getNextState().getName());
                         break;
                     }
                 }
             } else if (state.getType() == State.FINAL_STATE) {
-                for (State sourceState : flow.getStateListToOwnState(state)) {
+                for (State sourceState : fFlow.getStateListToOwnState(state)) {
                     Map sourceStateMap = new LinkedHashMap();
                     sourceStateMap.put("name", sourceState.getName());
                     if (sourceState.getType() == State.VIEW_STATE) {
                         sourceStateMap.put("view", sourceState.getView());
                     }
-                    for (Event event : sourceState.getEventList()) {
-                        EventHandler eventHandler = event.getEventHandler();
-                        if (eventHandler == null) {
-                            continue;
-                        }
-                        
-                        Map eventMap = new LinkedHashMap();
-                        String methodName = "";
-                        if (eventHandler.getClassName() == null) {
-                            methodName = flow.getActionClassName() + ":" +
-                                         eventHandler.getMethodName();
-                        } else {
-                            methodName = eventHandler.toString();
-                        }
-                        eventMap.put("method", methodName);
-                        
-                        if (event.getName().equals("Activity")) {
-                            sourceStateMap.put("activity", eventMap);
-                        } else if (event.getName().equals("Entry")) {
-                            sourceStateMap.put("entry", eventMap);
-                        } else if (event.getName().equals("Exit")) {
-                            sourceStateMap.put("exit", eventMap);
-                        }
+                    addBuiltinEventToMap(sourceState, sourceStateMap);
+                    finalStateMap.put("lastState", sourceStateMap);
+                }
+            } else if (state.getType() == State.VIEW_STATE
+                     || state.getType() == State.ACTION_STATE) {
+                // ファイナルステートへの遷移がある場合はなにもしない
+                boolean finalStateFlag = false;
+                for (Event event : state.getTransitionEventList()) {
+                    if (event.getNextState().getType() == State.FINAL_STATE) {
+                        finalStateFlag = true;
+                        break;
                     }
-                    stateMap.put("lastState", sourceStateMap);
+                }
+                if (finalStateFlag) {
+                    continue;
+                }
+                
+                Map stateMap = new LinkedHashMap();
+                stateMap.put("name", state.getName());
+                if (state.getType() == State.VIEW_STATE) {
+                    stateMap.put("view", state.getView());
+                }
+                addBuiltinEventToMap(state, stateMap);
+                addTransitionAndInternalEventToMap(state, stateMap);
+                
+                if (state.getType() == State.VIEW_STATE) {
+                    viewStateList.add(stateMap);
+                } else if (state.getType() == State.ACTION_STATE) {
+                    actionStateList.add(stateMap);
                 }
             } else {
                 continue;
             }
-            
-            yamlBuffer.append(Yaml.dump(stateMap, true));
+        }
+        
+        StringBuffer yamlBuffer = new StringBuffer();
+        if (firstStateMap.size() > 0) {
+            yamlBuffer.append(Yaml.dump(firstStateMap, true));
+            yamlBuffer.append("\n");
+        }
+        if (finalStateMap.size() > 0) {
+            yamlBuffer.append(Yaml.dump(finalStateMap, true));
+            yamlBuffer.append("\n");
+        }
+        if (viewStateList.size() > 0) {
+            viewStateMap.put("viewState", viewStateList);
+            yamlBuffer.append(Yaml.dump(viewStateMap, true));
+            yamlBuffer.append("\n");
+        }
+        if (actionStateList.size() > 0) {
+            actionStateMap.put("actionState", actionStateList);
+            yamlBuffer.append(Yaml.dump(actionStateMap, true));
             yamlBuffer.append("\n");
         }
         
         return formatYAMLString(yamlBuffer.toString());
+    }
+
+    private void addBuiltinEventToMap(State state, Map map) {
+        for (Event event : state.getEventList()) {
+            if (event.getType() != Event.BUILTIN_EVENT) {
+                continue;
+            }
+            EventHandler eventHandler = event.getEventHandler();
+            if (eventHandler == null) {
+                continue;
+            }
+            
+            Map eventMap = new LinkedHashMap();
+            String methodName = "";
+            if (eventHandler.getClassName() == null) {
+                methodName = fFlow.getActionClassName() + ":"
+                           + eventHandler.getMethodName();
+            } else {
+                methodName = eventHandler.toString();
+            }
+            eventMap.put("method", methodName);
+            
+            if (event.getName().equals("Activity")) {
+                map.put("activity", eventMap);
+            } else if (event.getName().equals("Entry")) {
+                map.put("entry", eventMap);
+            } else if (event.getName().equals("Exit")) {
+                map.put("exit", eventMap);
+            }
+        }
+    }
+
+    private void addTransitionAndInternalEventToMap(State state, Map map) {
+        List eventList = new ArrayList();
+        for (Event event : state.getEventList()) {
+            if (event.getType() != Event.TRANSITION_EVENT
+                && event.getType() != Event.INTERNAL_EVENT) {
+                continue;
+            }
+            
+            Map eventMap = new LinkedHashMap();
+            eventMap.put("event", event.getName());
+            eventMap.put("nextState", event.getNextState().getName());
+            EventHandler eventHandler = event.getEventHandler();
+            Map eventHandlerMap = new LinkedHashMap();
+            if (eventHandler != null) {
+                String methodName = "";
+                if (eventHandler.getClassName() == null) {
+                    methodName = fFlow.getActionClassName() + ":"
+                               + eventHandler.getMethodName();
+                } else {
+                    methodName = eventHandler.toString();
+                }
+                eventHandlerMap.put("method", methodName);
+                eventMap.put("action", eventHandlerMap);
+            }
+            
+            eventList.add(eventMap);
+        }
+        map.put("transition", eventList);
     }
     
     /**
@@ -101,19 +183,22 @@ public final class FlowMapper {
      * ・"--- \n" → 空文字列<br>
      * ・"\"" → 空文字列<br>
      * ・" !java.util.LinkedHashMap" → 空文字列<br>
+     * ・"-\n    " → "- "<br>
+     * ・": \n" → ":\n"<br>
      * ・最後の2連続改行 → ひとつの改行のみ<br>
      * 
      * @param yamlString YAML文字列
      * @return 整形したYAML文字列
      */
-    private static String formatYAMLString(String yamlString) {
+    private String formatYAMLString(String yamlString) {
         String result = yamlString;
         
         result = result.replace("\r\n", "\n");
         result = result.replace("--- \n", "");
         result = result.replace("\"", "");
         result = result.replace(" !java.util.LinkedHashMap", "");
-        
+        result = result.replaceAll("-\n *", "- ");
+        result = result.replace(": \n", ":\n");
         result = result.substring(0, result.length() - 1);
         
         return result;
