34cae7c90ecc442a4abe0e409a784b5f438a01df
MATSUFUJI Hideharu <matsufuji@iteman.jp>
2007-08-01 13:51:20 +0000

    - formatYAMLString メソッドをスーパークラスへ移動した。

--- com.piece_framework.piece_ide/src/com/piede_framework/piece_ide/flow_designer/mapper/AbstractStateMapper.java
+++ com.piece_framework.piece_ide/src/com/piede_framework/piece_ide/flow_designer/mapper/AbstractStateMapper.java
@@ -145,34 +145,4 @@ public abstract class AbstractStateMapper extends AbstractMapper {
         }
         return methodName;
     }
-    
-    /**
-     * YAML文字列を整形する.
-     * 以下の規則に従って整形する。<br>
-     * ・"\r\n" → \n<br>
-     * ・"--- \n" → 空文字列<br>
-     * ・"\"" → 空文字列<br>
-     * ・" !java.util.LinkedHashMap" → 空文字列<br>
-     * ・"-\n *" → "- "<br>
-     * ・": \n" → ":\n"<br>
-     * ・最後の2連続改行 → ひとつの改行のみ<br>
-     * 
-     * @param yamlString YAML文字列
-     * @return 整形したYAML文字列
-     */
-    protected String formatYAMLString(String yamlString) {
-        String result = yamlString;
-        
-        result = result.replace("\r\n", "\n");
-        result = result.replace("--- \n", "");
-        result = result.replace("\"", "");
-        result = result.replace(" !java.util.LinkedHashMap", "");
-        result = result.replaceAll("-\n *", "- ");
-        result = result.replace(": \n", ":\n");
-        if (result.length() >= 1) {
-            result = result.substring(0, result.length() - 1);
-        }
-        
-        return result;
-    }
 }
--- com.piece_framework.piece_ide/src/com/piede_framework/piece_ide/flow_designer/mapper/FlowMapper.java
+++ com.piece_framework.piece_ide/src/com/piede_framework/piece_ide/flow_designer/mapper/FlowMapper.java
@@ -54,34 +54,6 @@ public class FlowMapper extends AbstractMapper {
     }
 
     /**
-     * YAML文字列を整形する.
-     * 以下の規則に従って整形する。<br>
-     * ・"\r\n" → \n<br>
-     * ・"--- \n" → 空文字列<br>
-     * ・"\"" → 空文字列<br>
-     * ・" !java.util.LinkedHashMap" → 空文字列<br>
-     * ・"-\n *" → "- "<br>
-     * ・": \n" → ":\n"<br>
-     * ・最後の2連続改行 → ひとつの改行のみ<br>
-     * 
-     * @param yamlString YAML文字列
-     * @return 整形したYAML文字列
-     */
-    private String formatYAMLString(String yamlString) {
-        String result = yamlString;
-        
-        result = result.replace("\r\n", "\n");
-        result = result.replace("--- \n", "");
-        result = result.replace("\"", "");
-        result = result.replace(" !java.util.LinkedHashMap", "");
-        result = result.replaceAll("-\n *", "- ");
-        result = result.replace(": \n", ":\n");
-        result = result.substring(0, result.length() - 1);
-        
-        return result;
-    }
-
-    /**
      * モデルを取得する.
      * 
      * @param yaml YAML 文字列
