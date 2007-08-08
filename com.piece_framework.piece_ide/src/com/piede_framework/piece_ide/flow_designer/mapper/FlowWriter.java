9594406200e5b9be7b175f37ef2e302be1828de5
MATSUFUJI Hideharu <matsufuji@iteman.jp>
2007-08-08 13:27:37 +0000

    - 新規追加。

---
+++ com.piece_framework.piece_ide/src/com/piede_framework/piece_ide/flow_designer/mapper/FlowWriter.java
@@ -0,0 +1,27 @@
+package com.piede_framework.piece_ide.flow_designer.mapper;
+
+import org.eclipse.core.resources.IFile;
+
+import com.piece_framework.piece_ide.flow_designer.model.Flow;
+
+/**
+ * フローライター.
+ * FlowオブジェクトをYAMLファイルとシリアライズファイルに書き込む.
+ * 
+ * @author MATSUFUJI Hideharu
+ * @version 0.1.0
+ * @since 0.1.0
+ *
+ */
+public class FlowWriter {
+
+    /**
+     * FlowオブジェクトをYAMLファイルとシリアライズファイルに書き込む.
+     * 
+     * @param file YAMLファイル(.flowファイル)
+     * @param flow Flowオブジェクト
+     */
+    public static void write(IFile file, Flow flow) {
+        
+    }
+}
