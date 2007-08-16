b91b7b79214f053775d3a1a1736b928f2fc0846e
SUGIMOTO Seiichi <sugimoto@key-p.co.jp>
2007-08-16 09:31:05 +0000

    - 新規追加。

---
+++ com.piece.framework.piece_ide.wizard/src/com/piece_framework/piece_ide/wizard/project/NewPieceProjectWizardPage.java
@@ -0,0 +1,25 @@
+// $Id: NewPieceProjectWizardPage.java 9 2007-03-14 16:31:24Z sugimoto $
+package com.piece_framework.piece_ide.wizard.project;
+
+import org.eclipse.ui.dialogs.WizardNewProjectCreationPage;
+
+
+/**
+ * 新規�プロジェクトウィザードページ.
+ * 新規�プロジェクト作成�ウィザードページを管理する
+ * 
+ * @author Seiichi Sugimoto
+ * @version 0.1.0
+ * @since 0.1.0
+ * 
+ */
+public class NewPieceProjectWizardPage extends WizardNewProjectCreationPage {
+    
+    /**
+     * コンストラクタ.
+     * @param pageName ウィザードページ名
+     */
+    public NewPieceProjectWizardPage(String pageName) {
+        super(pageName);
+    }
+}
