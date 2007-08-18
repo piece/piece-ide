b954531cb58fdcdf929c61f2b61590884229f8be
MATSUFUJI Hideharu <matsufuji@iteman.jp>
2007-08-18 07:59:44 +0000

    - CheckStyle の警告に対応した。

--- com.piece.framework.piece_ide.wizard/src/com/piece_framework/piece_ide/wizard/plugin/Messages.java
+++ com.piece.framework.piece_ide.wizard/src/com/piece_framework/piece_ide/wizard/plugin/Messages.java
@@ -13,7 +13,7 @@ import java.util.ResourceBundle;
  */
 public final class Messages {
     private static final String BUNDLE_NAME =
-                "com.piece_framework.piece_ide.wizard.plugin.messages"; //$NON-NLS-1$
+        "com.piece_framework.piece_ide.wizard.plugin.messages"; //$NON-NLS-1$
 
     private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle
             .getBundle(BUNDLE_NAME);
