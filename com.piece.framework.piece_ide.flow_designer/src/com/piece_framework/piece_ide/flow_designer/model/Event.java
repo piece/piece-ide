0e57ea559330d3d04b2151965357ed71de8a61b0
MATSUFUJI Hideharu <matsufuji@iteman.jp>
2007-08-19 11:37:57 +0000

    - EventHandler クラスを Event クラスのインナークラスにした。

--- com.piece.framework.piece_ide.flow_designer/src/com/piece_framework/piece_ide/flow_designer/model/Event.java
+++ com.piece.framework.piece_ide.flow_designer/src/com/piece_framework/piece_ide/flow_designer/model/Event.java
@@ -31,6 +31,117 @@ public class Event extends AbstractModel implements Serializable {
     private EventHandler fGuardEventHandler;
     
     /**
+     * イベントハンドラクラス.
+     * 
+     * @author MATSUFUJI Hideharu
+     * @version 0.1.0
+     * @since 0.1.0
+     *
+     */
+    private final class EventHandler implements Serializable {
+
+        private static final long serialVersionUID = 8627376257429314590L;
+        
+        private String fClassName;
+        private String fMethodName;
+        
+        /**
+         * コンストラクタ.
+         * クラス名とメソッド名が ":" で接続された文字列からクラス名
+         * とメソッド名を分割して、設定する。
+         * 
+         * @param classAndMethodName クラス名 + ":" + メソッド名
+         */
+        private EventHandler(String classAndMethodName) {
+            if (classAndMethodName == null) {
+                return;
+            }
+            String[] arrayValue = classAndMethodName.split(":");
+            
+            String className = null;
+            String methodName = null;
+            
+            if (arrayValue.length == 2) {
+                className = arrayValue[0];
+                methodName = arrayValue[1];
+            } else if (arrayValue.length == 1) {
+                if (classAndMethodName.endsWith(":")) {
+                    className = arrayValue[0];
+                } else {
+                    methodName = arrayValue[0];
+                }
+            }
+            
+            setClassName(className);
+            setMethodName(methodName);
+        }
+        
+        /**
+         * クラス名を返す.
+         * 
+         * @return クラス名
+         */
+        private String getClassName() {
+            return fClassName;
+        }
+        
+        /**
+         * クラス名を設定する.
+         * 
+         * @param className クラス名
+         */
+        private void setClassName(String className) {
+            if (className != null && className.length() > 0) {
+                fClassName = className;
+            } else {
+                fClassName = null;
+            }
+        }
+
+        /**
+         * メソッド名を返す.
+         * 
+         * @return メソッド名
+         */
+        private String getMethodName() {
+            return fMethodName;
+        }
+        
+        /**
+         * メソッド名を設定する.
+         * 
+         * @param methodName メソッド名
+         */
+        private void setMethodName(String methodName) {
+            if (methodName != null && methodName.length() > 0) {
+                fMethodName = methodName;
+            } else {
+                fMethodName = null;
+            }
+        }
+
+        /**
+         * クラス名・メソッド名を ":" で接続した文字列を返す.
+         * 
+         * @return クラス名・メソッド名を ":" で接続した文字列 
+         * @see java.lang.Object#toString()
+         */
+        @Override
+        public String toString() {
+            String className = getClassName();
+            String methodName = getMethodName();
+            
+            if (className != null && className.length() > 0 
+                && methodName != null && methodName.length() > 0) {
+                return className + ":" + methodName;
+            } else if (methodName != null && methodName.length() > 0) {
+                return methodName;
+            }
+            return null;
+        }   
+    }
+    
+    /**
      * コンストラクタ.
      * イベントタイプは以下の定数のいずれかである必要があります。<br>
      * ビルトインイベント：BUILTIN_EVENT<br>
