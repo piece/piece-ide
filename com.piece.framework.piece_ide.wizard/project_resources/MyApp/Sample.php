badd29291b37f7f1064d1dfa77724271f7ab1ed6
MATSUFUJI Hideharu <matsufuji@iteman.jp>
2007-08-22 23:48:27 +0000

    - 新規追加。

---
+++ com.piece.framework.piece_ide.wizard/project_resources/MyApp/Sample.php
@@ -0,0 +1,58 @@
+<?php
+
+/**
+ * NtApp Sample.
+ *
+ * @package    MyApp
+ * @copyright  xxxx <xxxx@example.com>
+ * @license    
+ * @version    SVN: $Id$
+ * @since      File available since Release 1.0.0
+ */
+
+// {{{ MyApp_Sample
+
+/**
+ * MyApp Sample.
+ *
+ * @package    MyApp
+ * @copyright  xxxx <xxxx@example.com>
+ * @license    
+ * @version    Release: 1.0.0
+ * @since      Class available since Release 1.0.0
+ */
+class MyApp_Sample
+{
+
+    // {{{ properties
+
+    /**#@+
+     * @access public
+     */
+
+    /**#@-*/
+
+    /**#@+
+     * @access private
+     */
+
+    /**#@-*/
+
+    /**#@+
+     * @access public
+     */
+
+    /**#@-*/
+
+    /**#@+
+     * @access private
+     */
+
+    /**#@-*/
+
+    // }}}
+}
+
+// }}}
+
+?>
---
+++ com.piece.framework.piece_ide.wizard/project_resources/web/webapp/actions/ProtectedResourceAction.php
@@ -0,0 +1,116 @@
+<?php
+/* vim: set expandtab tabstop=4 shiftwidth=4: */
+
+/**
+ * PHP versions 4 and 5
+ *
+ * Copyright (c) 2007 KUBO Atsuhiro <iteman@users.sourceforge.net>,
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @package    Piece_Unity
+ * @copyright  2007 KUBO Atsuhiro <iteman@users.sourceforge.net>
+ * @license    http://www.opensource.org/licenses/bsd-license.php  BSD License (revised)
+ * @version    SVN: $Id$
+ * @since      File available since Release 1.0.0
+ */
+
+require_once 'Piece/Unity/Service/FlowAction.php';
+
+// {{{ ProtectedResourceAction
+
+/**
+ * Action class for the flow ProtectedResourceAction.
+ *
+ * @package    Piece_Unity
+ * @copyright  2007 KUBO Atsuhiro <iteman@users.sourceforge.net>
+ * @license    http://www.opensource.org/licenses/bsd-license.php  BSD License (revised)
+ * @version    Release: 1.0.0
+ * @since      Class available since Release 1.0.0
+ */
+class ProtectedResourceAction extends Piece_Unity_Service_FlowAction
+{
+
+    // {{{ properties
+
+    /**#@+
+     * @access public
+     */
+
+    /**#@-*/
+
+    /**#@+
+     * @access private
+     */
+
+    /**#@-*/
+
+    /**#@+
+     * @access public
+     */
+
+    function doActivityOnDisplayA()
+    {
+        $this->_setTitle();
+    }
+
+    function doActivityOnDisplayB()
+    {
+        $this->_setTitle();
+    }
+
+    function doActivityOnDisplayC()
+    {
+        $this->_setTitle();
+    }
+
+    /**#@-*/
+
+    /**#@+
+     * @access private
+     */
+
+    function _setTitle()
+    {
+        $viewElement = &$this->_context->getViewElement();
+        $viewElement->setElement('title', 'B.2. A resource which is protected by the above authentication service. *non-exclusive*');
+    }
+
+    /**#@-*/
+
+    // }}}
+}
+
+// }}}
+
+/*
+ * Local Variables:
+ * mode: php
+ * coding: iso-8859-1
+ * tab-width: 4
+ * c-basic-offset: 4
+ * c-hanging-comment-ender-p: nil
+ * indent-tabs-mode: nil
+ * End:
+ */
+?>
