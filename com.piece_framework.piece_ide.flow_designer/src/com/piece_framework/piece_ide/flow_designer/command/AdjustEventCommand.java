6312583fac269c2eeb5cfc827ea68f47fca574b4
MATSUFUJI Hideharu <matsufuji@iteman.jp>
2007-10-06 01:42:35 +0000

    - JavaDoc を整備した。

--- com.piece_framework.piece_ide.flow_designer/src/com/piece_framework/piece_ide/flow_designer/command/AdjustEventCommand.java
+++ com.piece_framework.piece_ide.flow_designer/src/com/piece_framework/piece_ide/flow_designer/command/AdjustEventCommand.java
@@ -12,22 +12,54 @@ import com.piece_framework.piece_ide.flow_designer.model.Event;
 import com.piece_framework.piece_ide.flow_designer.model.Flow;
 import com.piece_framework.piece_ide.flow_designer.model.State;
 
+/**
+ * イベント調整コマンド.
+ * ステート名に合わせて、すべてのイベント名及びイベントハンドラ名を調整する。
+ * 
+ * @author MATSUFUJI Hideharu
+ * @version 0.2.0
+ * @since 0.2.0
+ *
+ */
 public class AdjustEventCommand extends Command {
     private Flow fFlow;
     private Map<Event, Event> fEventMap;
     
+    /**
+     * コンストラクタ.
+     * 
+     * @param flow フロー
+     */
     public AdjustEventCommand(Flow flow) {
         fFlow = flow;
         fEventMap = new HashMap<Event, Event>();
     }
 
+    /**
+     * すべてのステートが保持するイベントが調整対象となるので、
+     * ユーザ問い合わせを行う.
+     * 
+     * @return コマンドを実行できるか
+     * @see org.eclipse.gef.commands.Command#canExecute()
+     */
     @Override
     public boolean canExecute() {
-        return MessageDialog.openQuestion(null, "イベントの調整",  // Adjust events
+        return MessageDialog.openQuestion(
+                null, "イベントの調整",  // Adjust events
                 "イベント名・イベントハンドラ名をステート名に合わせて更新します。\n"
                 + "よろしいですか？");
     }
 
+    /**
+     * ステート名に合わせて、すべてのイベント名及びイベントハンドラ名を調整する.
+     * {@link State#generateEventName(String)}は既に同名のイベントがある場合、
+     * 末尾に連番をセットする。そのため関連するステート名が変更されていないイベ
+     * ントに関して、イベント名を変更しようとすると連番がセットされてしまう。<br>
+     * そこでイベントは一度削除して、新しく作成する。<br>
+     * このため、随時ステートからイベントを取得するのが望ましい。
+     * 
+     * @see org.eclipse.gef.commands.Command#execute()
+     */
     @Override
     public void execute() {
         if (fFlow == null) {
@@ -48,10 +80,11 @@ public class AdjustEventCommand extends Command {
                     newEvent.setName(event.getName());
                 } else {
                     newEvent.setName(
-                        state.generateEventName(event.getNextState().getName()));
+                        state.generateEventName(
+                                event.getNextState().getName()));
                     newEvent.setNextState(event.getNextState());
                 }
-                System.out.println(event.getName() + "->" + newEvent.getName());
+                
                 if (event.getEventHandler() != null) {
                     boolean isNormalState =
                                 state.getType() == State.VIEW_STATE
@@ -82,6 +115,11 @@ public class AdjustEventCommand extends Command {
         }
     }
     
+    /**
+     * Mapオブジェクトの保持された旧イベントに差し替える.
+     * 
+     * @see org.eclipse.gef.commands.Command#undo()
+     */
     @Override
     public void undo() {
         for (State state : fFlow.getStateList()) {
