f55a8718416c0a41e55b8dc29c4c4d0ce5a1df15
MATSUFUJI Hideharu <matsufuji@iteman.jp>
2007-11-13 02:16:20 +0000

    - タブをスペースに変換した。 - package.html を追加した。

--- com.piece_framework.piece_ide.piece_unity_ide/src/com/piece_framework/piece_ide/piece_unity_ide/plugin/PieceUnityIdePlugin.java
+++ com.piece_framework.piece_ide.piece_unity_ide/src/com/piece_framework/piece_ide/piece_unity_ide/plugin/PieceUnityIdePlugin.java
@@ -14,17 +14,17 @@ import org.osgi.framework.BundleContext;
 public class PieceUnityIdePlugin extends AbstractUIPlugin {
 
     /** プラグインID. */
-	public static final String PLUGIN_ID
-	                 = "com.piece_framework.piece_ide.piece_unity_ide";
+    public static final String PLUGIN_ID
+                     = "com.piece_framework.piece_ide.piece_unity_ide";
 
     /** インスタンス. */
-	private static PieceUnityIdePlugin fPlugin;
-	
+    private static PieceUnityIdePlugin fPlugin;
+    
     /**
      * コンストラクタ.
      */
-	public PieceUnityIdePlugin() {
-	}
+    public PieceUnityIdePlugin() {
+    }
 
     /**
      * プラグインを開始する.
@@ -34,10 +34,10 @@ public class PieceUnityIdePlugin extends AbstractUIPlugin {
      * @see org.eclipse.ui.plugin.AbstractUIPlugin
      *          #start(org.osgi.framework.BundleContext)
      */
-	public void start(BundleContext context) throws Exception {
-		super.start(context);
-		fPlugin = this;
-	}
+    public void start(BundleContext context) throws Exception {
+        super.start(context);
+        fPlugin = this;
+    }
 
     /**
      * プラグインを停止する.
@@ -48,18 +48,17 @@ public class PieceUnityIdePlugin extends AbstractUIPlugin {
      * @see org.eclipse.ui.plugin.AbstractUIPlugin
      *          #stop(org.osgi.framework.BundleContext)
      */
-	public void stop(BundleContext context) throws Exception {
-		fPlugin = null;
-		super.stop(context);
-	}
+    public void stop(BundleContext context) throws Exception {
+        fPlugin = null;
+        super.stop(context);
+    }
 
     /**
      * 共有インスタンスを取得する.
      *
      * @return 共有インスタンス
      */
-	public static PieceUnityIdePlugin getDefault() {
-		return fPlugin;
-	}
-
+    public static PieceUnityIdePlugin getDefault() {
+        return fPlugin;
+    }
 }
