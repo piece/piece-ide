273e6e1672f92961455ba8e19fc59436ae150266
SUGIMOTO Seiichi <sugimoto@key-p.co.jp>
2007-12-06 14:49:45 +0000

    -フローエディタ上でコンテキストメニューを表示。

---
+++ com.piece_framework.piece_ide.piece_unity_ide/src/com/piece_framework/piece_ide/piece_unity_ide/action/PieceUnityAction.java
@@ -0,0 +1,36 @@
+package com.piece_framework.piece_ide.piece_unity_ide.action;
+
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.ui.IEditorActionDelegate;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.IWorkbenchPart;
+
+public class PieceUnityAction implements IEditorActionDelegate {
+
+	public PieceUnityAction() {
+		super();
+	}
+	
+	public void setActivePart(IAction action, IWorkbenchPart targetPart) {
+		
+	}
+
+	public void run(IAction action) {
+		
+	}
+
+	public void selectionChanged(ISelection selection) {
+		
+	}
+
+	public void selectionChanged(IAction action, ISelection selection) {
+	// TODO 自動生成されたメソッド・スタブ
+	
+	}
+	
+	public void setActiveEditor(IAction action, IEditorPart targetEditor) {
+		// TODO 自動生成されたメソッド・スタブ
+	
+	}
+}
