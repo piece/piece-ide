b09c8e4bbd6c611459f17b3c3cfcca91c545c87c
MATSUFUJI Hideharu <matsufuji@iteman.jp>
2007-12-30 03:39:31 +0000

    - ソースに $Id$ を付加した。

--- com.piece_framework.piece_ide.flow_designer/src/com/piece_framework/piece_ide/flow_designer/command/AbstractSetAttributeCommand.java
+++ com.piece_framework.piece_ide.flow_designer/src/com/piece_framework/piece_ide/flow_designer/command/AbstractSetAttributeCommand.java
@@ -1,3 +1,4 @@
+// $Id$
 package com.piece_framework.piece_ide.flow_designer.command;
 
 import java.lang.reflect.InvocationTargetException;
--- com.piece_framework.piece_ide.flow_designer/src/com/piece_framework/piece_ide/flow_designer/ui/editpart/NormalStateEditPart.java
+++ com.piece_framework.piece_ide.flow_designer/src/com/piece_framework/piece_ide/flow_designer/ui/editpart/NormalStateEditPart.java
@@ -1,3 +1,4 @@
+// $Id$
 package com.piece_framework.piece_ide.flow_designer.ui.editpart;
 
 import java.beans.PropertyChangeEvent;
