eba159bcef53999febc79a216d4a1890b4baa1fc
MATSUFUJI Hideharu <matsufuji@iteman.jp>
2007-12-30 03:59:04 +0000

    - タブをスペースに変換した。

--- com.piece_framework.piece_ide.piece_unity_ide/src/com/piece_framework/piece_ide/piece_unity_ide/action/PieceUnityAction.java
+++ com.piece_framework.piece_ide.piece_unity_ide/src/com/piece_framework/piece_ide/piece_unity_ide/action/PieceUnityAction.java
@@ -9,29 +9,29 @@ import org.eclipse.ui.IWorkbenchPart;
 
 public class PieceUnityAction implements IEditorActionDelegate {
 
-	public PieceUnityAction() {
-		super();
-	}
-	
-	public void setActivePart(IAction action, IWorkbenchPart targetPart) {
-		
-	}
+    public PieceUnityAction() {
+        super();
+    }
+    
+    public void setActivePart(IAction action, IWorkbenchPart targetPart) {
+        
+    }
 
-	public void run(IAction action) {
-		
-	}
+    public void run(IAction action) {
+        
+    }
 
-	public void selectionChanged(ISelection selection) {
-		
-	}
+    public void selectionChanged(ISelection selection) {
+        
+    }
 
-	public void selectionChanged(IAction action, ISelection selection) {
-	// TODO 自動生成されたメソッド・スタブ
-	
-	}
-	
-	public void setActiveEditor(IAction action, IEditorPart targetEditor) {
-		// TODO 自動生成されたメソッド・スタブ
-	
-	}
+    public void selectionChanged(IAction action, ISelection selection) {
+    // TODO 自動生成されたメソッド・スタブ
+    
+    }
+    
+    public void setActiveEditor(IAction action, IEditorPart targetEditor) {
+        // TODO 自動生成されたメソッド・スタブ
+    
+    }
 }
