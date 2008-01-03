8864ebdc0a8968e1a5265e9037e5de523ee3f0c7
MATSUFUJI Hideharu <matsufuji@iteman.jp>
2008-01-03 13:00:06 +0000

    - 一覧を Table を使用するようにした。 - Field モデルを追加した。

---
+++ com.piece_framework.piece_ide.form_designer/src/com/piece_framework/piece_ide/form_designer/Field.java
@@ -0,0 +1,50 @@
+// $Id$
+package com.piece_framework.piece_ide.form_designer;
+
+public final class Field {
+    private String fName;
+    private String fDescription;
+    private boolean fRequired;
+    private boolean fForceValidation;
+    private String fMessage;
+
+    public String getName() {
+        return fName;
+    }
+
+    public void setName(final String name) {
+        fName = name;
+    }
+
+    public String getDescription() {
+        return fDescription;
+    }
+
+    public void setDescription(final String description) {
+        fDescription = description;
+    }
+
+    public boolean isRequired() {
+        return fRequired;
+    }
+
+    public void setRequired(final boolean required) {
+        fRequired = required;
+    }
+
+    public boolean isForceValidation() {
+        return fForceValidation;
+    }
+
+    public void setForceValidation(final boolean forceValidation) {
+        fForceValidation = forceValidation;
+    }
+
+    public String getMessage() {
+        return fMessage;
+    }
+
+    public void setMessage(final String message) {
+        fMessage = message;
+    }
+}
--- com.piece_framework.piece_ide.form_designer/src/com/piece_framework/piece_ide/form_designer/FieldsPage.java
+++ com.piece_framework.piece_ide.form_designer/src/com/piece_framework/piece_ide/form_designer/FieldsPage.java
@@ -1,10 +1,19 @@
 // $Id$
 package com.piece_framework.piece_ide.form_designer;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.jface.viewers.ArrayContentProvider;
+import org.eclipse.jface.viewers.ILabelProviderListener;
+import org.eclipse.jface.viewers.ITableLabelProvider;
+import org.eclipse.jface.viewers.TableViewer;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Tree;
+import org.eclipse.swt.widgets.Table;
+import org.eclipse.swt.widgets.TableColumn;
 import org.eclipse.ui.forms.DetailsPart;
 import org.eclipse.ui.forms.IManagedForm;
 import org.eclipse.ui.forms.MasterDetailsBlock;
@@ -19,35 +28,68 @@ public class FieldsPage extends FormPage {
     
     public class FieldsBlock extends MasterDetailsBlock {
 
+        private static final int NAME_WIDTH = 100; 
+        private static final int DESCRIPTION_WIDTH = 100; 
+        
         @Override
         protected void createMasterPart(
                         IManagedForm managedForm,
                         Composite parent) {
             FormToolkit toolkit = managedForm.getToolkit();
             
-            Section section = toolkit.createSection(parent, Section.TITLE_BAR | Section.DESCRIPTION);
+            Section section = toolkit.createSection(parent, 
+                                    Section.TITLE_BAR | Section.DESCRIPTION);
             section.setText("フィールド一覧");
             section.setDescription("編集するフィールドを選択してください。");
             
             Composite composite = toolkit.createComposite(section);
             composite.setLayout(new FillLayout());
-            Tree tree = toolkit.createTree(composite, SWT.NONE);
+            Table table = toolkit.createTable(
+                                    composite, 
+                                    SWT.SINGLE | SWT.FULL_SELECTION);
+            table.setHeaderVisible(true);
+            table.setLinesVisible(true);
+            TableColumn column1 = new TableColumn(table, SWT.NULL);
+            column1.setText("名前");
+            column1.setWidth(NAME_WIDTH);
+            TableColumn column2 = new TableColumn(table, SWT.NULL);
+            column2.setText("説明");
+            column2.setWidth(DESCRIPTION_WIDTH);
+            TableViewer viewer = new TableViewer(table);
+            viewer.setContentProvider(new ArrayContentProvider());
+            viewer.setLabelProvider(new ITableLabelProvider() {
+                public Image getColumnImage(Object element, int columnIndex) {
+                    return null;
+                }
+
+                public String getColumnText(Object element, int columnIndex) {
+                    Field field = (Field) element;
+                    if (columnIndex == 0) {
+                        return field.getName();
+                    } else if (columnIndex == 1) {
+                        return field.getDescription();
+                    }
+                    return "";
+                }
+
+                public void addListener(ILabelProviderListener listener) {
+                }
+
+                public void dispose() {
+                }
+
+                public boolean isLabelProperty(
+                                    Object element, 
+                                    String property) {
+                    return false;
+                }
+
+                public void removeListener(ILabelProviderListener listener) {
+                }
+            });
+            viewer.setInput(createSampleData());
             
             section.setClient(composite);
-            
-//            SectionPart part = new SectionPart(section);
-//            managedForm.addPart(part);
-//            section.setLayout(new FormLayout());
-//            section.setLayoutData(new GridData(GridData.FILL_BOTH));
-            
-//            Composite container = managedForm.getToolkit().createComposite(parent);
-//            container.setLayout(FormLayoutFactory.createMasterGridLayout(false, 1));
-//            container.setLayoutData(new GridData(GridData.FILL_BOTH));
-//            fSection = createMasterSection(managedForm, container);
-//            managedForm.addPart(fSection);
-//            Section section = fSection.getSection();
-//            section.setLayout(FormLayoutFactory.createClearGridLayout(false, 1));
-//            section.setLayoutData(new GridData(GridData.FILL_BOTH));
         }
 
         @Override
@@ -61,7 +103,28 @@ public class FieldsPage extends FormPage {
             // TODO 自動生成されたメソッド・スタブ
             
         }
-    
+        
+        private List createSampleData() {
+            List<Field> fields = new ArrayList<Field>();
+
+            Field field1 = new Field();
+            field1.setName("name");
+            field1.setDescription("氏名");
+            field1.setRequired(true);
+            field1.setForceValidation(true);
+            field1.setMessage("%description%は必須項目です。");
+            fields.add(field1);
+ 
+            Field field2 = new Field();
+            field2.setName("age");
+            field2.setDescription("年齢");
+            field2.setRequired(false);
+            field2.setForceValidation(false);
+            field2.setMessage("%description%は必須項目です。");
+            fields.add(field2);
+
+            return fields;
+        }
     }
     
     public FieldsPage(FormEditor editor) {
