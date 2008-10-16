1968bb600420c7181eca9f2473244b52b1513717
MATSUFUJI Hideharu <matsufuji@iteman.jp>
2008-10-16 05:46:19 +0000

    - branches/1.0 に移動した。

---
+++ com.piece_framework.piece_ide.piece_orm.mapper.editor/src/com/piece_framework/piece_ide/piece_orm/mapper/Piece_ORM_MapperEditorPlugin.java
@@ -0,0 +1,26 @@
+package com.piece_framework.piece_ide.piece_orm.mapper;
+
+import org.openarchitectureware.xtext.AbstractXtextEditorPlugin;
+import org.openarchitectureware.xtext.LanguageUtilities;
+import org.osgi.framework.BundleContext;
+
+public class Piece_ORM_MapperEditorPlugin extends AbstractXtextEditorPlugin {
+   private static Piece_ORM_MapperEditorPlugin plugin;
+   public static Piece_ORM_MapperEditorPlugin getDefault() {
+      return plugin;
+   }
+
+   private Piece_ORM_MapperUtilities utilities = new Piece_ORM_MapperUtilities();
+   public LanguageUtilities getUtilities() {
+      return utilities;
+   }
+
+   public Piece_ORM_MapperEditorPlugin() {
+      plugin = this;
+   }
+
+   public void stop(BundleContext context) throws Exception {
+      super.stop(context);
+      plugin = null;
+   }
+}
