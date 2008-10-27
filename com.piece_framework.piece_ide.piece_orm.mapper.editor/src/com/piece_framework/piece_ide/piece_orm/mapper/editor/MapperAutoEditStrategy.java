8f6c8eb603a24f117ecee005b077630ab915fe33
MATSUFUJI Hideharu <matsufuji@iteman.jp>
2008-10-27 04:25:22 +0000

    - 閉じブレースがない場合のみ、自動的に閉じブレースを入力するようにした。

--- com.piece_framework.piece_ide.piece_orm.mapper.editor/src/com/piece_framework/piece_ide/piece_orm/mapper/editor/MapperAutoEditStrategy.java
+++ com.piece_framework.piece_ide.piece_orm.mapper.editor/src/com/piece_framework/piece_ide/piece_orm/mapper/editor/MapperAutoEditStrategy.java
@@ -6,6 +6,7 @@ import org.eclipse.jface.text.BadLocationException;
 import org.eclipse.jface.text.DefaultIndentLineAutoEditStrategy;
 import org.eclipse.jface.text.DocumentCommand;
 import org.eclipse.jface.text.IDocument;
+import org.eclipse.jface.text.IRegion;
 import org.eclipse.jface.text.TextUtilities;
 import org.eclipse.ui.texteditor.AbstractDecoratedTextEditorPreferenceConstants;
 
@@ -43,11 +44,12 @@ public class MapperAutoEditStrategy extends DefaultIndentLineAutoEditStrategy {
         }
 
         try {
-            if (document.getChar(command.offset - 1) == '{') {
-                command.text = command.text
-                               + fIndent
-                               + TextUtilities.getDefaultLineDelimiter(document)
-                               + "}";
+            if (getBraceCount(document) > 0) {
+                command.text = command.text +
+                               fIndent +
+                               TextUtilities.getDefaultLineDelimiter(document) +
+                               "}" +
+                               TextUtilities.getDefaultLineDelimiter(document);
                 command.shiftsCaret = false;
                 command.caretOffset = command.offset + fIndent.length() + 1;
             }
@@ -63,5 +65,89 @@ public class MapperAutoEditStrategy extends DefaultIndentLineAutoEditStrategy {
             return TextUtilities.equals(delimiters, text) > -1;
         }
         return false;
+    }
+
+    private int getBraceCount(IDocument document) throws BadLocationException {
+        int offset = 0;
+        int bracketCount= 0;
+        while (offset < document.getLength()) {
+            char currentChar = document.getChar(offset);
+            offset++;
+            switch (currentChar) {
+                case '/' :
+                    offset = getCommentEnd(document,
+                                           offset
+                                           );
+                    break;
+                case '{' :
+                    bracketCount++;
+                    break;
+                case '}' :
+                    bracketCount--;
+                    break;
+                case '"' :
+                case '\'' :
+                    offset = getStringEnd(document,
+                                          offset,
+                                          currentChar
+                                          );
+                    break;
+                default :
+            }
+        }
+        return bracketCount;
+    }
+
+    private int getCommentEnd(IDocument document,
+                              int offset
+                              ) throws BadLocationException {
+        int commentEnd = document.getLength();
+        if (offset < document.getLength()) {
+            char next = document.getChar(offset);
+            if (next == '*') {
+                commentEnd = getMultiLineCommentEnd(document,
+                                                    offset + 1
+                                                    );
+            } else if (next == '/') {
+                IRegion region = document.getLineInformation(document.getLineOfOffset(offset));
+                commentEnd = region.getOffset() + region.getLength();
+            }
+        }
+        return commentEnd;
+    }
+
+    private int getMultiLineCommentEnd(final IDocument document,
+                                       final int startOffset
+                                       ) throws BadLocationException {
+        int offset = startOffset;
+        while (offset < document.getLength()) {
+            char currentChar = document.getChar(offset);
+            offset++;
+            if (currentChar == '*') {
+                if (offset < document.getLength()
+                    && document.getChar(offset) == '/'
+                    ) {
+                    return offset + 1;
+                }
+            }
+        }
+        return document.getLength();
+    }
+
+    private int getStringEnd(final IDocument document,
+                             final int startOffset,
+                             final char quotationChar
+                             ) throws BadLocationException {
+        int offset = startOffset;
+        while (offset < document.getLength()) {
+            char currentChar = document.getChar(offset);
+            offset++;
+            if (currentChar == '\\') {
+                offset++;
+            } else if (currentChar == quotationChar) {
+                return offset;
+            }
+        }
+        return document.getLength();
     }
 }
