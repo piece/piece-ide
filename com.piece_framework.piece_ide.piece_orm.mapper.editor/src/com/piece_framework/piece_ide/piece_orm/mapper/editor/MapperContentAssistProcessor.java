9b84e3f7064e50af8b85be2ce504459115d55c9f
MATSUFUJI Hideharu <matsufuji@iteman.jp>
2008-11-06 13:25:34 +0000

    - 補完候補をスーパークラスの internalComputeProposals() メソッドで作成するようにした。 - 既に入力されているルールを補完候補から除外するようにした。 - 不要になったクラスを削除した。

--- com.piece_framework.piece_ide.piece_orm.mapper.editor/src/com/piece_framework/piece_ide/piece_orm/mapper/editor/MapperContentAssistProcessor.java
+++ com.piece_framework.piece_ide.piece_orm.mapper.editor/src/com/piece_framework/piece_ide/piece_orm/mapper/editor/MapperContentAssistProcessor.java
@@ -2,66 +2,22 @@
 package com.piece_framework.piece_ide.piece_orm.mapper.editor;
 
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
-import java.util.Set;
+import java.util.Map;
 
-import org.eclipse.emf.ecore.EObject;
 import org.eclipse.jface.text.contentassist.ICompletionProposal;
-import org.eclipse.swt.graphics.Image;
-import org.openarchitectureware.xtext.Assignment;
-import org.openarchitectureware.xtext.CrossReference;
-import org.openarchitectureware.xtext.Element;
-import org.openarchitectureware.xtext.EnumLiteral;
-import org.openarchitectureware.xtext.Keyword;
 import org.openarchitectureware.xtext.LanguageUtilities;
-import org.openarchitectureware.xtext.StringRule;
-import org.openarchitectureware.xtext.XtextLog;
 import org.openarchitectureware.xtext.editor.AbstractXtextEditor;
 import org.openarchitectureware.xtext.editor.contentassist.XTextModelContentAssist;
-import org.openarchitectureware.xtext.editor.contentassist.XtextCompletionProposal;
-import org.openarchitectureware.xtext.editor.contentassist.codeassist.Proposal;
 import org.openarchitectureware.xtext.parser.model.NodeUtil;
 import org.openarchitectureware.xtext.parser.parsetree.Node;
 
 public class MapperContentAssistProcessor extends XTextModelContentAssist {
-    private static final String CONTENT_ASSIST_EXTENSIONS = "ContentAssist";
-    private LanguageUtilities fLangUtil;
-
-    private class Grammar {
-        private Node fNode;
-        public Grammar(Node node) {
-            fNode = node;
-        }
-
-        public boolean hasCrossReference() {
-            if (fNode.getGrammarElement() instanceof Assignment) {
-                Assignment assignment = (Assignment) fNode.getGrammarElement();
-                return assignment.getToken() instanceof CrossReference;
-            }
-            return false;
-        }
-
-        public boolean hasStringRule() {
-            for (EObject element = fNode.getGrammarElement();
-                 element != null;
-                 element = element.eContainer()
-                 ) {
-                if (element instanceof StringRule) {
-                    return true;
-                }
-            }
-            return false;
-        }
-    }
-
     public MapperContentAssistProcessor(LanguageUtilities langUtil,
                                         AbstractXtextEditor editor
                                         ) {
         super(langUtil, editor);
-        fLangUtil = langUtil;
     }
 
     @Override
@@ -69,131 +25,66 @@ public class MapperContentAssistProcessor extends XTextModelContentAssist {
                                                           int offset,
                                                           Node node
                                                           ) {
-        if (node == null) {
+        ICompletionProposal[] proposals = super.internalComputeProposals(wholetext, offset, node);
+        if (proposals == null) {
             return null;
         }
 
-        NodeForContentAssist lastComplete = new NodeForContentAssist(node, offset);
-        if (lastComplete.hasParent()) {
-            Grammar grammar = new Grammar(lastComplete.getParent());
-            if (grammar.hasCrossReference() && grammar.hasStringRule()) {
-                lastComplete = new NodeForContentAssist(lastComplete.getParent());
-            }
+        Node lastComplete = NodeUtil.getNodeBeforeOffset(node, offset);
+        if (lastComplete == null) {
+            return proposals;
         }
-
-        ProposalParameter parameter = ProposalParameter.create(wholetext,
-                                                               offset,
-                                                               lastComplete
-                                                               );
-        List<Proposal> proposals = new ArrayList<Proposal>(createProposals(parameter));
-        proposals = sortProposals(proposals);
-
-        List<ICompletionProposal> completionProposals = 
-            createCompletionProposals(proposals,
-                                      parameter.getPrefix()
-                                      );
-
-        return completionProposals.toArray(new ICompletionProposal[completionProposals.size()]);
-    }
-
-    private List<Proposal> createProposals(ProposalParameter parameter) {
-        Set<Element> followUps = NodeUtil.getPossibleFollows(fLangUtil.getXtextFile(),
-                                                             parameter.getLastComplete()
-                                                             );
-        List<Proposal> proposals = new ArrayList<Proposal>();
-        for (Element element : followUps) {
-            try {
-                proposals.addAll(handleElement(element,
-                                               parameter.getLastComplete(),
-                                               parameter.getPrefix()
-                                               ));
-            } catch (RuntimeException e) {
-                if (e.getMessage() == null) {
-                    XtextLog.logInfo(e.getClass().getSimpleName() + " occurred",
-                                     e
-                                     );
-                } else {
-                    XtextLog.logInfo(e.getMessage());
-                }
-            }
-        }
-
-        for (Proposal proposal : proposals) {
-            proposal.setStartReplace(parameter.getStartReplace());
-            proposal.setEndReplace(parameter.getEndReplace());
-            proposal.setApplyPrefixFilter(parameter.isApplyPrefixFilter());
-        }
-
-        return proposals;
-    }
-
-    private List<ICompletionProposal> createCompletionProposals(List<Proposal> proposals,
-                                                                String prefix
-                                                                ) {
-        List<ICompletionProposal> completionProposals = new ArrayList<ICompletionProposal>();
-        for (Proposal proposal : proposals) {
-            if (!proposal.isApplyPrefixFilter()
-                || prefix == null
-                || proposal.getToInsert().toLowerCase().startsWith(prefix.toLowerCase())
-                ) {
-                ICompletionProposal completionProposal =
-                    new XtextCompletionProposal(proposal,
-                                                fLangUtil.getImage(proposal.getImage())
-                                                );
-                completionProposals.add(completionProposal);
-            }
-        }
-        return completionProposals;
-    }
-
-    private List<Proposal> handleElement(Element element,
-                                         Node lastComplete,
-                                         String prefix
-                                         ) {
-        List<Proposal> proposals = null;
-        if (element instanceof Assignment) {
-            proposals = handleAssignment((Assignment) element, lastComplete, prefix);
-        } else if (element instanceof Keyword) {
-            proposals = handleKeyword((Keyword) element, lastComplete, prefix);
-        } else if (element instanceof EnumLiteral) {
-            proposals = handleEnumLiteral((EnumLiteral) element, lastComplete, prefix);
-        } else {
-            proposals = handleOther(element, lastComplete, prefix);
-        }
-
-        if (proposals == null) {
-            proposals = Collections.emptyList();
-        }
-
-        return proposals;
-    }
-
-    private List<Proposal> sortProposals(List<Proposal> proposals) {
-        Object sortedProposals = fLangUtil.invokeExtension(CONTENT_ASSIST_EXTENSIONS,
-                                                           "sortProposals",
-                                                           proposals
-                                                           );
-        if (!(sortedProposals instanceof Collections)) {
+        boolean insideElement = lastComplete.getEnd() >= offset;
+        if (insideElement) {
             return proposals;
         }
 
-        if (sortedProposals == proposals) {
-            Collections.sort(proposals, new Comparator<Proposal>() {
-                public int compare(Proposal o1, Proposal o2) {
-                    if (o1.isApplyPrefixFilter() != o2.isApplyPrefixFilter()) {
-                        if (o1.isApplyPrefixFilter()) {
-                            return -1;
-                        } else {
-                            return 1;
-                        }
-                    }
-                    return o1.getLabel().compareTo(o2.getLabel());
-                }
-            });
-        } else {
-            proposals = (List<Proposal>) sortedProposals;
+        Map<String, String> parentRules = new HashMap<String, String>();
+        parentRules.put("Mapper", "Mapper");
+        parentRules.put("Method", "Method");
+        parentRules.put("Association", "Association");
+        parentRules.put("InnerAssociation", "InnerAssociation");
+        parentRules.put("LinkTable", "LinkTable");
+
+        Node parent = lastComplete.getParent();
+        while (parent != null
+               && (!parentRules.containsKey(parent.getModelElement().eClass().getName())
+                   || parent.getStart() > offset
+                   || offset > parent.getEnd()
+                   )) {
+            parent = parent.getParent();
+        }
+        if (parent == null) {
+            return proposals;
         }
 
-        return proposals;
+        List<ICompletionProposal> removeProposals = new ArrayList<ICompletionProposal>();
+        for (Object childNode : parent.getChildren()) {
+            String nodeText = NodeUtil.getText(wholetext, (Node) childNode).trim();
+            if (nodeText.startsWith("association") || nodeText.startsWith("method")) {
+                continue;
+            }
+            for (ICompletionProposal proposal : proposals) {
+                if (nodeText.startsWith(proposal.getDisplayString())) {
+                    removeProposals.add(proposal);
+                }
+            }
+        }
+
+        List<ICompletionProposal> newProposals = new ArrayList<ICompletionProposal>();
+        for (ICompletionProposal proposal : proposals) {
+            boolean add = true;
+            for (ICompletionProposal removeProposal : removeProposals) {
+                if (proposal == removeProposal) {
+                    add = false;
+                    break;
+                }
+            }
+            if (add) {
+                newProposals.add(proposal);
+            }
+        }
+
+        return newProposals.toArray(new ICompletionProposal[0]);
     }
 }
