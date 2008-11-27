037d7c75ff6e8a87800297d6d2d5a13eb87a59e0
MATSUFUJI Hideharu <matsufuji@iteman.jp>
2008-11-27 13:46:37 +0000

    - プロジェクト名の変更に対応した。

---
+++ com.piece_framework.piece_ide.piece_orm.mapper.oaw/src/com/piece_framework/piece_ide/piece_orm/mapper/oaw/parser/XtextParser.java
@@ -0,0 +1,12 @@
+
+package com.piece_framework.piece_ide.piece_orm.mapper.oaw.parser;
+
+import java.io.InputStream;
+
+public class XtextParser extends GenParser {
+
+	public XtextParser(InputStream in) {
+		super(in);
+	}
+
+}
