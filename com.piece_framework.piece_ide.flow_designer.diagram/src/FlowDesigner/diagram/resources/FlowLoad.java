9384e89e21d13163589d9f8f1fb818a0e8d22fad
MATSUFUJI Hideharu <matsufuji@iteman.jp>
2009-01-27 05:54:09 +0000

    - TransitionEventLoad クラスを追加した。

--- com.piece_framework.piece_ide.flow_designer.diagram/src/FlowDesigner/diagram/resources/FlowLoad.java
+++ com.piece_framework.piece_ide.flow_designer.diagram/src/FlowDesigner/diagram/resources/FlowLoad.java
@@ -2,15 +2,11 @@
 
 package FlowDesigner.diagram.resources;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import org.eclipse.emf.ecore.EObject;
 
 import FlowDesigner.Flow;
-import FlowDesigner.NamedState;
 
 class FlowLoad extends AbstractLoad {
     @Override
@@ -21,80 +17,21 @@ class FlowLoad extends AbstractLoad {
             return;
         }
 
-        createStates((Flow) eObject, map);
-        createEvents((Flow) eObject, map);
-    }
+        Flow flow = (Flow) eObject;
 
-    private void createStates(Flow flow,
-                              Map<?, ?> flowMap
-                              ) {
         InitialStateLoad initialStateLoad = new InitialStateLoad();
-        initialStateLoad.load(flow, flowMap);
+        initialStateLoad.load(flow, map);
 
         FinalStateLoad finalStateLoad = new FinalStateLoad();
-        finalStateLoad.load(flow, flowMap);
+        finalStateLoad.load(flow, map);
         
         ViewStateLoad viewStateLoad = new ViewStateLoad();
-        viewStateLoad.load(flow, flowMap);
+        viewStateLoad.load(flow, map);
 
         ActionStateLoad actionStateLoad = new ActionStateLoad();
-        actionStateLoad.load(flow, flowMap);
-    }
+        actionStateLoad.load(flow, map);
 
-    @SuppressWarnings("unchecked")
-    private List<Map<?, ?>> getStateList(Map<?, ?> flow,
-                                         Object key
-                                         ) {
-        if (flow.get(key) == null) {
-            return null;
-        }
-
-        ArrayList<Map<?, ?>> states = null;
-        if (flow.get(key) instanceof HashMap) {
-            ArrayList<Map<?, ?>> list = new ArrayList<Map<?, ?>>();
-            HashMap<?, ?> stateAttributes = (HashMap<?, ?>) flow.get(key);
-            list.add(stateAttributes);
-            states = list;
-        } else {
-            ArrayList<Map<?, ?>> list = new ArrayList<Map<?, ?>>();
-            list.addAll((ArrayList<HashMap<?, ?>>) flow.get(key));
-            states = list;
-        }
-        return states;
-    }
-
-    private void createEvents(Flow flow,
-                              Map<?, ?> flowMap
-                              ) {
-        if (flowMap.get("firstState") != null) {
-            HashMap<String, String> eventAttributes = new HashMap<String, String>();
-            eventAttributes.put("event", "(FirstState)");
-            eventAttributes.put("nextState", (String) flowMap.get("firstState"));
-
-            EventLoad load = new EventLoad();
-            load.load(flow.getInitialState(), eventAttributes);
-        }
-
-        List<Map<?, ?>> viewStates = getStateList(flowMap, "viewState");
-        List<Map<?, ?>> actionStates = getStateList(flowMap, "actionState");
-        List<Map<?, ?>> states = new ArrayList<Map<?, ?>>();
-        if (viewStates != null) {
-            states.addAll(viewStates);
-        }
-        if (actionStates != null) {
-            states.addAll(actionStates);
-        }
-        for (Map<?, ?> stateAttributes : states) {
-            ArrayList<?> events = (ArrayList<?>) stateAttributes.get("transition");
-            if (events == null) {
-                continue;
-            }
-
-            NamedState state = flow.findStateByName((String) stateAttributes.get("name"));
-            for (HashMap<?, ?> eventAttributes : events.toArray(new HashMap<?, ?>[0])) {
-                EventLoad load = new EventLoad();
-                load.load(state, eventAttributes);
-            }
-        }
+        TransitionEventLoad transitionEventLoad = new TransitionEventLoad();
+        transitionEventLoad.load(flow, map);
     }
 }
