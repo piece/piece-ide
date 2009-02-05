7bef6dfac6ba769546643de064ea9f33c64b0e5e
MATSUFUJI Hideharu <matsufuji@iteman.jp>
2009-02-05 02:48:23 +0000

    - r814 の変更を取り消した。

--- com.piece_framework.piece_ide.flow_designer.diagram/src/FlowDesigner/diagram/providers/FlowDesignerIconProvider.java
+++ com.piece_framework.piece_ide.flow_designer.diagram/src/FlowDesigner/diagram/providers/FlowDesignerIconProvider.java
@@ -1,66 +1,22 @@
 package FlowDesigner.diagram.providers;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.eclipse.core.runtime.IAdaptable;
 import org.eclipse.gmf.runtime.common.core.service.AbstractProvider;
 import org.eclipse.gmf.runtime.common.core.service.IOperation;
 import org.eclipse.gmf.runtime.common.ui.services.icon.GetIconOperation;
 import org.eclipse.gmf.runtime.common.ui.services.icon.IIconProvider;
-import org.eclipse.gmf.runtime.emf.core.util.EObjectAdapter;
 import org.eclipse.swt.graphics.Image;
-
-import FlowDesigner.diagram.part.FlowDesignerDiagramEditorPlugin;
-import FlowDesigner.impl.ActionStateImpl;
-import FlowDesigner.impl.EventImpl;
-import FlowDesigner.impl.FinalStateImpl;
-import FlowDesigner.impl.FlowImpl;
-import FlowDesigner.impl.InitialStateImpl;
-import FlowDesigner.impl.ViewStateImpl;
 
 /**
  * @generated
  */
 public class FlowDesignerIconProvider extends AbstractProvider implements
         IIconProvider {
-    private Map<Class, Image> fIconMap;
 
     /**
-     * @generated NOT
+     * @generated
      */
     public Image getIcon(IAdaptable hint, int flags) {
-        if (hint instanceof EObjectAdapter) {
-            Object realObject = ((EObjectAdapter) hint).getRealObject();
-            if (fIconMap == null) {
-                fIconMap = new HashMap<Class, Image>();
-                fIconMap.put(FlowImpl.class,
-                             FlowDesignerDiagramEditorPlugin
-                                 .getBundledImageDescriptor("icons/FlowDesigner.gif").createImage()
-                             );
-                fIconMap.put(InitialStateImpl.class,
-                             FlowDesignerDiagramEditorPlugin
-                                 .getBundledImageDescriptor("icons/InitialState.gif").createImage()
-                             );
-                fIconMap.put(FinalStateImpl.class,
-                             FlowDesignerDiagramEditorPlugin
-                                 .getBundledImageDescriptor("icons/FinalState.gif").createImage()
-                             );
-                fIconMap.put(ViewStateImpl.class,
-                             FlowDesignerDiagramEditorPlugin
-                                 .getBundledImageDescriptor("icons/ViewState.gif").createImage()
-                             );
-                fIconMap.put(ActionStateImpl.class,
-                             FlowDesignerDiagramEditorPlugin
-                                 .getBundledImageDescriptor("icons/ActionState.gif").createImage()
-                             );
-                fIconMap.put(EventImpl.class,
-                             FlowDesignerDiagramEditorPlugin
-                                 .getBundledImageDescriptor("icons/Event.gif").createImage()
-                             );
-            }
-            return fIconMap.get(realObject.getClass());
-        }
         return FlowDesigner.diagram.providers.FlowDesignerElementTypes
                 .getImage(hint);
     }
