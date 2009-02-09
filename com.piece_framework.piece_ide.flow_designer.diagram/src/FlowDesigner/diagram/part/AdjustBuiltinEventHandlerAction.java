a53d2735a92754716836ecac334d501868120f0e
MATSUFUJI Hideharu <matsufuji@iteman.jp>
2009-02-09 02:06:08 +0000

    - フローのコンテキストメニューに "Adjust Built-in Event Handler" を追加した。

---
+++ com.piece_framework.piece_ide.flow_designer.diagram/src/FlowDesigner/diagram/part/AdjustBuiltinEventHandlerAction.java
@@ -0,0 +1,29 @@
+// $Id$
+package FlowDesigner.diagram.part;
+
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.ui.IObjectActionDelegate;
+import org.eclipse.ui.IWorkbenchPart;
+
+public class AdjustBuiltinEventHandlerAction implements IObjectActionDelegate {
+
+    public AdjustBuiltinEventHandlerAction() {
+        // TODO Auto-generated constructor stub
+    }
+
+    public void setActivePart(IAction action, IWorkbenchPart targetPart) {
+        // TODO Auto-generated method stub
+
+    }
+
+    public void run(IAction action) {
+        // TODO Auto-generated method stub
+
+    }
+
+    public void selectionChanged(IAction action, ISelection selection) {
+        // TODO Auto-generated method stub
+
+    }
+}
