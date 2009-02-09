857c740e347b0643399639e2aaaa52a1a4553d29
MATSUFUJI Hideharu <matsufuji@iteman.jp>
2009-02-09 08:48:25 +0000

    - ビルトインイベントのハンドラをステート名に合わせて調整する機能を実装した。(Ticket #175)

--- com.piece_framework.piece_ide.flow_designer.diagram/src/FlowDesigner/diagram/edit/commands/AdjustBuiltinEventHandlerCommand.java
+++ com.piece_framework.piece_ide.flow_designer.diagram/src/FlowDesigner/diagram/edit/commands/AdjustBuiltinEventHandlerCommand.java
@@ -10,6 +10,7 @@ import org.eclipse.gmf.runtime.emf.type.core.commands.EditElementCommand;
 import org.eclipse.gmf.runtime.emf.type.core.requests.IEditCommandRequest;
 
 import FlowDesigner.Flow;
+import FlowDesigner.NamedState;
 
 public class AdjustBuiltinEventHandlerCommand extends EditElementCommand {
     public AdjustBuiltinEventHandlerCommand(String label,
@@ -23,6 +24,19 @@ public class AdjustBuiltinEventHandlerCommand extends EditElementCommand {
     protected CommandResult doExecuteWithResult(IProgressMonitor monitor,
                                                 IAdaptable info
                                                 ) throws ExecutionException {
+        Flow flow = (Flow) getElementToEdit();
+        for (NamedState state: flow.getStates()) {
+            if (state.getEntry() != null) {
+                state.setEntry("doEntryOn" + state.getName());
+            }
+            if (state.getActivity() != null) {
+                state.setActivity("doActivtyOn" + state.getName());
+            }
+            if (state.getExit() != null) {
+                state.setExit("doExitOn" + state.getName());
+            }
+        }
+
         return CommandResult.newOKCommandResult();
     }
 }
