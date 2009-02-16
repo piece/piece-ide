b14ef2f8929e33ad4e00a5525f926a58fdefe2bd
MATSUFUJI Hideharu <matsufuji@iteman.jp>
2009-02-16 18:14:16 +0900

    - フロー定義ファイルのリネーム、移動のときにフローデザイナーが開いていた場合、フローデザイナーを閉じないようにした。

--- com.piece_framework.piece_ide.flow_designer.diagram/src/FlowDesigner/diagram/resource/DiagramFile.java
+++ com.piece_framework.piece_ide.flow_designer.diagram/src/FlowDesigner/diagram/resource/DiagramFile.java
@@ -6,8 +6,15 @@ import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.core.runtime.Path;
 import org.eclipse.emf.common.util.URI;
+import org.eclipse.ui.IEditorInput;
+import org.eclipse.ui.IEditorReference;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.part.FileEditorInput;
 
+import FlowDesigner.diagram.part.FlowDesignerDiagramEditor;
 import FlowDesigner.diagram.part.FlowDesignerDiagramEditorPlugin;
 import FlowDesigner.diagram.part.FlowDesignerDiagramEditorUtil;
 
@@ -63,6 +70,9 @@ public class DiagramFile {
                                                              newDiagramURI,
                                                              flowURI
                                                              );
+                    DiagramFile.this.refreshEditorInput(oldDiagramFile,
+                                                        fDiagramFile
+                                                        );
                     oldDiagramFile.delete(true, new NullProgressMonitor());
                 } catch (CoreException e) {
                     FlowDesignerDiagramEditorPlugin.getInstance().logError(
@@ -109,5 +119,29 @@ public class DiagramFile {
         fFlowFile = flowFile;
 
         fDiagramFile = getDiagramFile();
+    }
+
+    private void refreshEditorInput(IFile oldDiagramFile,
+                                    IFile newDiagramFile
+                                    ) throws PartInitException {
+        IEditorInput oldEditorInput = new FileEditorInput(oldDiagramFile);
+        IEditorInput newEditorInput = new FileEditorInput(newDiagramFile);
+
+        for (IWorkbenchWindow window: PlatformUI.getWorkbench().getWorkbenchWindows()) {
+            for (IWorkbenchPage page: window.getPages()) {
+                IEditorReference[] editorReferences = page.findEditors(oldEditorInput,
+                                                                       null,
+                                                                       IWorkbenchPage.MATCH_INPUT
+                                                                       );
+                for (IEditorReference editorReference: editorReferences) {
+                    if (editorReference.getEditor(true) instanceof FlowDesignerDiagramEditor) {
+                        FlowDesignerDiagramEditor editor = (FlowDesignerDiagramEditor) editorReference.getEditor(true);
+                        editor.init(editor.getEditorSite(),
+                                    newEditorInput
+                                    );
+                    }
+                }
+            }
+        }
     }
 }
