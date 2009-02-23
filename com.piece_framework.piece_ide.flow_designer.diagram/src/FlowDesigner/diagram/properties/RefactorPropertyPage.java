f7d5c6511e58e7c977509b849fdcb7aecf8d2269
MATSUFUJI Hideharu <matsufuji@iteman.jp>
2009-02-23 15:14:46 +0900

    - プロジェクトのプロパティーで Activitiy イベントハンドラを自動更新するかどうかを設定できるようにした。

---
+++ com.piece_framework.piece_ide.flow_designer.diagram/src/FlowDesigner/diagram/properties/RefactorPropertyPage.java
@@ -0,0 +1,72 @@
+package FlowDesigner.diagram.properties;
+
+import java.io.IOException;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.ProjectScope;
+import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.jface.preference.BooleanFieldEditor;
+import org.eclipse.jface.preference.FieldEditorPreferencePage;
+import org.eclipse.jface.preference.IPersistentPreferenceStore;
+import org.eclipse.ui.IWorkbenchPropertyPage;
+import org.eclipse.ui.preferences.ScopedPreferenceStore;
+
+import FlowDesigner.diagram.part.FlowDesignerDiagramEditorPlugin;
+
+public class RefactorPropertyPage extends FieldEditorPreferencePage implements IWorkbenchPropertyPage {
+    private BooleanFieldEditor fAutoUpdateActivityEventHandler;
+    private IProject fProject;
+
+    @Override
+    protected void createFieldEditors() {
+        fAutoUpdateActivityEventHandler =
+            new BooleanFieldEditor("autoUpdateActivityEventHandler",
+                                   "Activityイベントハンドラをステート名に合わせてを自動的に変更する",
+                                   getFieldEditorParent()
+                                   );
+        addField(fAutoUpdateActivityEventHandler);
+    }
+
+    @Override
+    public boolean performOk() {
+        IPersistentPreferenceStore store = (IPersistentPreferenceStore) getPreferenceStore();
+        if (store == null) {
+            return false;
+        }
+
+        store.setValue(getClass().getName(),
+                       System.currentTimeMillis()
+                       );
+        store.setValue("autoUpdateActivityEventHandler",
+                       fAutoUpdateActivityEventHandler.getBooleanValue()
+                       );
+        try {
+            store.save();
+        } catch (IOException e) {
+            FlowDesignerDiagramEditorPlugin.getInstance().logError("Unable to save property", //$NON-NLS-1$
+                                                                   e
+                                                                   );
+            return false;
+        }
+
+        return true;
+    }
+
+    public IAdaptable getElement() {
+        return fProject;
+    }
+
+    public void setElement(IAdaptable element) {
+        if (element instanceof IProject) {
+            fProject = (IProject) element;
+            ScopedPreferenceStore store =
+                new ScopedPreferenceStore(new ProjectScope(fProject),
+                                          FlowDesignerDiagramEditorPlugin.ID
+                                          );
+            store.setDefault("autoUpdateActivityEventHandler",
+                             false
+                             );
+            setPreferenceStore(store);
+        }
+    }
+}
