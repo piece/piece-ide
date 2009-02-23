fb6b6fdb5924cd081d89c2d78aac19a580a6b7cb
MATSUFUJI Hideharu <matsufuji@iteman.jp>
2009-02-23 10:40:21 +0900

    - 新規追加。

---
+++ com.piece_framework.piece_ide.flow_designer.diagram/src/FlowDesigner/diagram/preferences/GeneralPreferencePage.java
@@ -0,0 +1,15 @@
+// $Id$
+package FlowDesigner.diagram.preferences;
+
+import org.eclipse.gmf.runtime.common.ui.preferences.AbstractPreferencePage;
+import org.eclipse.swt.widgets.Composite;
+
+public class GeneralPreferencePage extends AbstractPreferencePage {
+    @Override
+    protected void addFields(Composite parent) {
+    }
+
+    @Override
+    protected void initHelp() {
+    }
+}
